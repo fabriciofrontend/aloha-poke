c1540093e0c71234383cbb67350d5c2b5d9e3859
Fabrício Lopes <fabriciogabriel04@gmail.com>
2023-09-15 00:47:56 +0100

    adjustments on responsive layout :tada:

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
+//Ativando o menu mobile
 function mostrarMenu() {
-  $("header nav#nav-esq ul#menu-principal").css("display", "flex");
-  $("header nav#nav-esq ul#menu-principal").addClass(
+  $("nav#nav-esq ul.menu-principal").css("display", "flex");
+  $("nav#nav-esq ul.menu-principal").addClass(
     "animate_animated animate_fadeInRight animate_slow"
   );
 
-  $("header nav#nav-esq ul#icone-menu li#menu").css("display", "none");
-  $("header nav#nav-esq ul#icone-menu li#menuX").css("display", "flex");
+  $("nav#nav-esq ul#icone-menu li#menu").css("display", "none");
+  $("nav#nav-esq ul#icone-menu li#menuX").css("display", "flex");
 }
 
 function esconderMenu() {
-  $("header nav#nav-esq ul#menu-principal").css("display", "none");
+  $("nav#nav-esq ul.menu-principal").css("display", "none");
 
-  $("header nav#nav-esq ul#icone-menu li#menu").css("display", "flex");
-  $("header nav#nav-esq ul#icone-menu  li#menuX").css("display", "none");
+  $("nav#nav-esq ul#icone-menu li#menu").css("display", "flex");
+  $("nav#nav-esq ul#icone-menu  li#menuX").css("display", "none");
 }
 
 //chamar função
 let controle = true;
 
-$("header nav#nav-esq ul#icone-menu").click(function () {
-  if ((controle = true)) {
-    //pra dizer que a função mostrarrMenu é true
+$("nav#nav-esq ul#icone-menu").click(function() {
+  if (controle == true) {
+    //pra dizer que a função mostrar Menu é true
     mostrarMenu();
     controle = false; // passa a false pq ele tem que se preparar para chamar de novo
   } else {
