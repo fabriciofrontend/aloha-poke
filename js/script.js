37949a731536e0996b0850e174c6d5423dd26652
Fabrício Lopes <fabriciogabriel04@gmail.com>
2023-09-24 20:27:55 +0100

    adjustments menu mobile :tada:

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function mostrarMenu() {
   $("header nav#nav-esq ul.menu-principal").css("display", "flex");
   $("header nav#nav-esq ul.menu-principal").addClass(
-    "animate_animated animate_fadeInRight animate_slow"
+    'animate__animated animate__fadeInRight animate__slow'
   );
 
   $("header nav#nav-esq ul#icone-menu li#menu").css("display", "none");
