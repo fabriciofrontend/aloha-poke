d587d51f89a040c275c34a969e7c324f3ca439be
Fabrício Lopes <fabriciogabriel04@gmail.com>
2023-09-25 18:35:03 +0100

    animations :tada:

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function mostrarMenu() {
   $("header nav#nav-esq ul.menu-principal").css("display", "flex");
   $("header nav#nav-esq ul.menu-principal").addClass(
-    'animate__animated animate__fadeInRight animate__slow'
+    "animate__animated animate__fadeInRight animate__slow"
   );
 
   $("header nav#nav-esq ul#icone-menu li#menu").css("display", "none");
@@ -23,7 +23,7 @@ $("header nav#nav-esq ul#icone-menu").click(function () {
   if (controle == true) {
     //pra dizer que a função mostrar Menu é true
     mostrarMenu();
-    controle = false; // passa a false pq ele tem que se preparar para chamar de novo
+    controle = false; //passa a false pq ele tem que se preparar para chamar de novo
   } else {
     esconderMenu();
     controle = true;
